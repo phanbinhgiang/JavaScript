97ac5933af2df7d5049c41c7f6061f8384077c0c
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-09-28 16:42:36 +0700

    update form validate part#1

---
+++ Form-Validation/validator.js
@@ -0,0 +1,49 @@
+function Validator(options) {
+  function validate(inputElement, rule) {
+    var errorElement = inputElement.parentElement.querySelector('.form-message');
+    var errorMessage = rule.test(inputElement.value);
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      inputElement.parentElement.classList.add('invalid');
+    } else {
+      errorElement.innerText = '';
+      inputElement.parentElement.classList.remove('invalid');
+    }
+  }
+  var formElement = document.getElementById(options.form);
+  if (formElement) {
+    options.rules.forEach(function (rule) {
+      var inputElement = document.querySelector(rule.selector);
+      if (inputElement) {
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        };
+
+        inputElement.oninput = function () {
+          var errorElement = inputElement.parentElement.querySelector('.form-message');
+          errorElement.innerText = '';
+          inputElement.parentElement.classList.remove('invalid');
+        };
+      }
+    });
+  }
+}
+
+Validator.isRequired = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.trim() ? undefined : 'Vui long nhap truong nay';
+    },
+  };
+};
+
+Validator.isEmail = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value) ? undefined : 'Truong nay phai la email';
+    },
+  };
+};
